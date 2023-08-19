77b0f027caa4dcb4592c0928c7fdf30a61e7c563
Koyo Kinoshita <58977709+kouyou13@users.noreply.github.com>
2023-08-19 17:17:00 +0900

    #4終了

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # React_tutorial_2
-#02終了
+#04終了
 以下のサイトを参考
 https://qiita.com/yassun-youtube/items/2ae26050efd2133c2286run
 
--- src/List.js
+++ src/List.js
@@ -1,8 +1,24 @@
-export const List = ({title}) => {
+const LANGUAGES = [
+  'C',
+  'C++',
+  'Java',
+  'JavaScript',
+  'Python',
+  'Perl',
+  'PHP',
+  'Go'
+]
+// export const List = ({title}) => {
+export const List = ({}) => {
   return (
     <div>
-      <h4>{title}</h4>
-      <div>リストです</div>
+      {/* <h4>{title}</h4> */}
+      {/* <div>リストです</div> */}
+      {
+        LANGUAGES.map((lang, index) => {
+          return <div key={index}>{ lang }</div>
+        })
+      }
     </div>
   )
 }
